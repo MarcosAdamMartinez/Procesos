7937302a54164120986b9eab714e84c90d674b98
Marcos Martinez <zerobananaboy@gmail.com>
2025-09-23 14:01:44 +0200

    Desarrollo clase ejecución procesos

--- src/psp/procesos/EjecProceso.java
+++ src/psp/procesos/EjecProceso.java
@@ -1,32 +1,59 @@
 package psp.procesos;
 
+import java.io.File;
 
 public class EjecProceso {
 
-    public void ejecutarPB(String comando){
+    public void ejecutarPB(){
 
         ProcessBuilder pb;
-        Process proceso;
-        //hola
+        Process process;
 
         try{
 
-            pb = new ProcessBuilder();
-            proceso = pb.start();
+            pb = new ProcessBuilder("gnome-terminal","");
+            File log = new File("salida.log");
+            pb.redirectOutput(log);
+            pb.redirectError(log);
+            process = pb.start();
+            int retorno = process.waitFor();
 
-
+            System.out.println("La ejecución de "+pb.command()+" devuelve "+retorno);
+            System.out.println("Las variable de entorno del proceso son: "+pb.environment());
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
 
     }
 
+    public void ejecutarRun(String[] comando){
+
+        Runtime runtime;
+        Process process;
+
+        try{
+
+            runtime = Runtime.getRuntime();
+            process = runtime.exec(comando);
+            synchronized (process){
+                process.wait(5000);
+            }
+            process.destroy();
+
+        } catch (Exception e){
+            throw new RuntimeException();
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        String comando = "";
+        String comando = "gnome-terminal";
+//        String comando2 = "notepad";
         EjecProceso ep = new EjecProceso();
-        ep.ejecutarPB(comando);
+        ep.ejecutarPB();
 
+        //ep.ejecutarRun(args);
     }
 
 }
