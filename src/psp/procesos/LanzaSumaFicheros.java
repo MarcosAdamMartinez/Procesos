18638a6da12ea5b4c4f295f2076b49914c51dfba
Marcos Martinez <zerobananaboy@gmail.com>
2025-10-07 14:20:23 +0200

    Cosas de clase avanzadas algo

--- src/psp/procesos/LanzaSumaFicheros.java
+++ src/psp/procesos/LanzaSumaFicheros.java
@@ -1,12 +1,102 @@
 package psp.procesos;
 
+import com.sun.source.doctree.SystemPropertyTree;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class LanzaSumaFicheros {
 
     private static final String[] filenames = {"cuenta1.txt","cuenta2.txt","cuenta3.txt","cuenta4.txt","cuenta5.txt","cuenta6.txt","cuenta7.txt","cuenta8.txt","cuenta9.txt","cuenta10.txt"};
 
-    public static void main(String[] args) {
+    public static final String packetPath = "psp.procesos.SumaUnFichero";
 
+    public static <lanzaSumaFicheros> void main(String[] args) throws IOException, InterruptedException {
 
+        ProcessBuilder pb = null;
+        File file;
+
+        List<Process> listaFicherosGenerados = new ArrayList<>();
+        for (String fileName : filenames){
+            file = new File(fileName);
+            if (!file.exists()){
+                pb = new ProcessBuilder("java", "-cp", System.getProperty("java.class.path"), packetPath + ".GeneraFicheroConNumeros", fileName);
+                listaFicherosGenerados.add(pb.start());
+            }
+        }
+
+        while (!listaFicherosGenerados.isEmpty()){
+            Process procesoFichero;
+            Iterator<Process> iterator = listaFicherosGenerados.iterator();
+            while (iterator.hasNext()){
+                procesoFichero = iterator.next();
+                if (!procesoFichero.isAlive()){
+                    iterator.remove();
+                }
+            }
+        }
+
+        LanzaSumaFicheros lsf = new LanzaSumaFicheros();
+        lsf.sumaSerie();
+    }
+    
+
+    public void sumaSerie() throws IOException, InterruptedException{
+        long total = 0;
+
+        Process proceso;
+        InputStream entrada;
+
+        ProcessBuilder pb = null;
+        for (String fileName : filenames){
+            pb = new ProcessBuilder("java", "-cp", System.getProperty("java.class.path"), packetPath + ".SumaUnFichero", fileName);
+            proceso = pb.start();
+            entrada = proceso.getInputStream();
+
+            String linea = new String(entrada.readAllBytes()).trim();
+            long valor = Long.parseLong(linea);
+
+            entrada.close();
+            total += valor;
+        }
+
+        System.out.printf("En total es: %20d\n", total);
+    }
+
+    public void sumaParalelo() throws IOException, InterruptedException{
+        long total = 0;
+
+        Process[] procesos = new Process[filenames.length];
+        InputStream[] entradas = new InputStream[filenames.length];
+
+        ProcessBuilder pb = null;
+        for (int pos=0; pos< filenames.length; pos++) {
+            pb = new ProcessBuilder("java", "-cp", System.getProperty("java.class.path"), packetPath + ".SumaUnFichero", filenames[pos]);
+            procesos[pos] = pb.start();
+            entradas[pos] = procesos[pos].getInputStream();
+
+        }
+        boolean algunVivo = true;
+        while (algunVivo){
+            algunVivo = false;
+
+            for (int pos=0; pos< filenames.length; pos++){
+                if (procesos[pos]==null)continue;
+                if (entradas[pos].available()>0){
+                    String linea = new String(entradas[pos].readAllBytes()).trim();
+                    long valor = Long.parseLong(linea);
+
+                    procesos[pos] = null;
+                    entradas[pos].close();
+                    total += valor;
+                }
+            }
+        }
 
     }
 
